52e7f64c1545f6d366f23973f3ead3407e562a13
davideb <davidebenedittis@gmail.com>
2016-11-06 18:19:11 +0100

    completed

--- app.js
+++ app.js
@@ -7,6 +7,36 @@ angular.module('LunchCheck', [])
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
 
+  $scope.check = function () {
+    if (!$scope.food)
+    {
+      $scope.message = 'Please enter data first';
+      $scope.messageStyle = "red";
+      $scope.inputStyle = "invalid";
+    }
+    else {
+      $scope.messageStyle = "green";
+      $scope.inputStyle = "valid";
+
+      var items = $scope.food.split(',');
+      var realItems = 0;
+
+      for (var i = 0; i < items.length; ++i) {
+        if(items[i].trim())
+        {
+          realItems++;
+        }
+      }
+
+      if (realItems<=3) {
+        $scope.message = 'Enjoy';
+      }
+      else {
+        $scope.message = 'Too much!';
+      }
+    }
+  };
+
 }
 
 })();
